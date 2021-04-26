eb61f4bcda1da1f0aea24729c9ad0a6d37320eac
Rajeev Gupta <rrpit9@gmail.com>
2021-04-26 18:31:27 +0530

    membership

---
+++ app/Model/Membership.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Membership extends Model
+{
+    use SoftDeletes;
+
+    protected $hidden = [
+        'deleted_at', 'updated_at','created_at',
+    ];
+}
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,7 @@ class CreateUsersTable extends Migration
             $table->string('mobile',20);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->bigInteger('membershipId');
             $table->string('otp',10);
             $table->tinyInteger('subscribed')->default(0)->comment('1:Subscribed ,0:Un-Subscribed');
             $table->tinyInteger('status')->comment('1:Active,0:In-Active')->default(1);
