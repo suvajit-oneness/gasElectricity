8aa983dec40a2591c6eab80a76b01f9b660faa29
Rajeev Gupta <rrpit9@gmail.com>
2021-04-26 16:50:43 +0530

    DB

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -36,6 +36,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
         });
+        
         $data = [
             [
                 'user_type' => 1,
