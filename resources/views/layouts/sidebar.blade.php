10f9c98f02957907e69a4f891175cb4eed5c2946
Rajeev Gupta <rajeev@onenesstechs.in>
2021-09-24 14:20:23 +0000

    Req

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -11,16 +11,6 @@
                         <li class="nav-divider"> Menu </li>
                         
                         <li class="nav-item">
-                                <a class="nav-link" href="javascript:void(0)" data-toggle="collapse" aria-expanded="{{request()->routeIs('supplier.setting.*')?'true':'false'}}" data-target="#supplierSettings-6" aria-controls="supplierSettings-6"><i class="fas fa-fw fa-file"></i> Settings </a>
-                                <div id="supplierSettings-6" class="collapse submenu {{request()->routeIs('supplier.setting.*')?'show':''}}">
-                                    <ul class="nav flex-column">
-                                        <li class="nav-item {{request()->routeIs('supplier.setting.form')?'active':''}}">
-                                            <a class="nav-link" href="{{route('supplier.setting.form')}}">Forms</a>
-                                        </li>
-                                    </ul>
-                                </div>
-                        </li> 
-                        <li class="nav-item">
                             <a class="nav-link {{request()->routeIs('home')?'active':''}}" href="{{route('home')}}"><i class="fa fa-fw fa-user-circle"></i>Dashboard</a>
                         </li>
 
@@ -83,7 +73,8 @@
                                         <li class="nav-item {{request()->routeIs('admin.setting.points')?'active':''}}">
                                             <a class="nav-link" href="{{route('admin.setting.points')}}">Points</a>
                                         </li>
-                                        <li class="nav-item {{request()->routeIs('admin.setting.about_us')?'active':''}}">
+                                        <li class="                                        <li class="nav-item {{request()->routeIs('supplier.setting.form')?'active':''}}">
+nav-item {{request()->routeIs('admin.setting.about_us')?'active':''}}">
                                             <a class="nav-link" href="{{route('admin.setting.about_us')}}">About us</a>
                                         </li>
                                         <li class="nav-item {{request()->routeIs('admin.setting.whychooseus')?'active':''}}">
@@ -122,7 +113,16 @@
                                 </div>
                             </li>
                             <li class="nav-divider">Setting</li>
-                            
+                            <li class="nav-item">
+                                <a class="nav-link" href="javascript:void(0)" data-toggle="collapse" aria-expanded="{{request()->routeIs('supplier.setting.*')?'true':'false'}}" data-target="#supplierSettings-6" aria-controls="supplierSettings-6"><i class="fas fa-fw fa-file"></i> Settings </a>
+                                <div id="supplierSettings-6" class="collapse submenu {{request()->routeIs('supplier.setting.*')?'show':''}}">
+                                    <ul class="nav flex-column">
+                                        <li class="nav-item {{request()->routeIs('supplier.setting.form')?'active':''}}">
+                                            <a class="nav-link" href="{{route('supplier.setting.form')}}">Forms</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </li> 
                         <!-- Supplier Sidebar End-->
                         <!-- Customer Sidebar -->
                         @elseif(Auth::user()->user_type == 3)
